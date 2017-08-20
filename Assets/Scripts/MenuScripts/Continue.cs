00658459d129d570426704d6d32dd5e9abb042e7
Francis <f.joseph.stephens@gmail.com>
2017-08-21 10:51:39 +1200

    VR life

---
+++ Assets/Scripts/MenuScripts/Continue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Continue : MonoBehaviour, IPointerClickHandler
+{
+    public int sceneIndexToLoad;
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        PlayerPrefs.DeleteAll();
+        GameManager.Instance.SwitchScene(sceneIndexToLoad);
+    }
+
+}
